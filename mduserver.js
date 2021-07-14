66dd0e05eefae6f55f284fa9edf2286000df4cf0
spss20 <sppro.20@gmail.com>
2021-07-14 21:49:29 +0530

    MDU on TCP/ip

--- mduserver.js
+++ mduserver.js
@@ -1,13 +1,56 @@
 const express = require('express');
+const SerialPort = require('serialport');
 const app = express()
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+var ByteBuffer = require("bytebuffer");
 
+//Serial
+const serialPort = require('serialport')
+const utils = require('./utils');
+const { arch } = require('os');
+const { Logger } = require('mongodb');
+const { hexStringToByte, byteToHexString } = require('./utils');
+const { clearTimeout } = require('timers');
+
+
+var lastPacketTime = -1;
+var delay = 70;
+var responseTimeout;
+
+serialPort.list().then(ports => {
+  ports.forEach(function (port) {
+    console.log(port.path);
+    console.log(port.pnpId);
+    console.log(port.manufacturer);
+  });
+});
+
+const printer = new SerialPort('COM16', {
+  baudRate: 9600,
+}, (error) => {
+  console.log(error)
+})
+
+const dispenser = new SerialPort('COM11', {
+  baudRate: 5787,
+  parity: 'even'
+}, (error) => {
+  console.log(error)
+})
+
+var lastPacket = [];
+
+// const array = new ArrayBuffer(1);
+// const buffer = new Uint8Array(array)
+// buffer[0]  = 0x51;
+// dispenser.write(buffer)
+
+app.use(express.static('public'))
 
 server.listen(3000, function () {
   console.log("Server listening on port 3000")
 });
-
 
 io.on('connection', socket => {
   console.log("Client Connected")
@@ -15,17 +58,49 @@ io.on('connection', socket => {
 
   //Read dispenser serial data
   socket.on("dispenser" , data => {
-    socket.broadcast.emit("dispenser" , data);
-    console.log(data);
+    // socket.broadcast.emit("dispenser" , data);
+    console.log("Received App: " , data);
+    dispenser.write(hexStringToByte(data))
+    lastPacket = []
+    console.log("Before Packet",lastPacket);
   })
-
 
   socket.on("nozzleReader" , data => {
-    socket.broadcast.emit("nozzleReader" , data);
+    // socket.broadcast.emit("nozzleReader" , data);
       console.log(data);
   })
-  socket.on('disconnect', () => { console.log("Client Disconnected") });
+
+  socket.on("printer" , data => {
+    const buffer = utils.hexStringToByte(data);
+    console.log(buffer)
+    if(buffer != null){
+        printer.write(buffer)
+    }
+  })
+  socket.on('disconnect', () => { 
+    console.log("Client Disconnected")
+    lastPacket = []
+    lastPacketTime = -1;
+    clearTimeout(responseTimeout)
+    responseTimeout = null;
+   });
+
 });
 
+dispenser.on('data', function(data) {
+ 
+  if(lastPacketTime == -1 || (new Date().getTime() - lastPacketTime) > delay){
 
+     responseTimeout = setTimeout(() => {
+        const buffer = Buffer.concat(lastPacket);
+        lastPacket = []
+        console.log("Length"  , buffer.length)
+        io.emit("dispenser", byteToHexString(buffer))
+      }, 500);
+
+  }
+
+  lastPacketTime = new Date().getTime()
+  lastPacket.push(data)
+})
 
--- utils.js
+++ utils.js
@@ -1,43 +1,73 @@
 module.exports = class utils {
-   static getCurrentDate(){
+    static getCurrentDate() {
         const date = new Date();
-        const dateString = date.getFullYear() + '-' + date.getMonth() + '-' + 
-        date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes();
+        const dateString = date.getFullYear() + '-' + date.getMonth() + '-' +
+            date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes();
         return dateString;
     }
 
-   static timeDifference(current, previous) {
+    static timeDifference(current, previous) {
 
         var msPerMinute = 60 * 1000;
         var msPerHour = msPerMinute * 60;
         var msPerDay = msPerHour * 24;
         var msPerMonth = msPerDay * 30;
         var msPerYear = msPerDay * 365;
-    
+
         var elapsed = current - previous;
-    
+
         if (elapsed < msPerMinute) {
-             return Math.round(elapsed/1000) + ' seconds';   
+            return Math.round(elapsed / 1000) + ' seconds';
         }
-    
+
         else if (elapsed < msPerHour) {
-             return Math.round(elapsed/msPerMinute) + ' minutes';   
+            return Math.round(elapsed / msPerMinute) + ' minutes';
         }
-    
-        else if (elapsed < msPerDay ) {
-             return Math.round(elapsed/msPerHour ) + ' hours';   
+
+        else if (elapsed < msPerDay) {
+            return Math.round(elapsed / msPerHour) + ' hours';
         }
-    
+
         else if (elapsed < msPerMonth) {
-            return 'approximately ' + Math.round(elapsed/msPerDay) + ' days';   
+            return 'approximately ' + Math.round(elapsed / msPerDay) + ' days';
         }
-    
+
         else if (elapsed < msPerYear) {
-            return 'approximately ' + Math.round(elapsed/msPerMonth) + ' months';   
+            return 'approximately ' + Math.round(elapsed / msPerMonth) + ' months';
         }
-    
+
         else {
-            return 'approximately ' + Math.round(elapsed/msPerYear ) + ' years';   
+            return 'approximately ' + Math.round(elapsed / msPerYear) + ' years';
         }
     }
+
+    static byteToHexString(uint8arr) {
+        if (!uint8arr) {
+          return '';
+        }
+        
+        var hexStr = '';
+        for (var i = 0; i < uint8arr.length; i++) {
+          var hex = (uint8arr[i] & 0xff).toString(16);
+          hex = (hex.length === 1) ? '0' + hex : hex;
+          hexStr += hex;
+          hexStr += " ";
+        }
+        
+        return hexStr.toUpperCase();
+      }
+      
+      static hexStringToByte(str) {
+        if (!str) {
+          return new Uint8Array();
+        }
+        
+        str = str.replace(/\s/g, "");
+        var a = [];
+        for (var i = 0, len = str.length; i < len; i+=2) {
+          a.push(parseInt(str.substr(i,2),16));
+        }
+        
+        return new Uint8Array(a);
+      }
 }
